c14aab67edd4f4bc1131f3cf0c34256e082a27ad
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-01-08 22:12:34 +0300

    digtal GPIO Lib

---
+++ Lib/GPIOLib/GPIO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.GPIOLib
+{
+    using System.Device.Gpio;
+    public class GPIO
+    {
+        GpioController _controller;
+        public GPIO(GpioController controller)
+        {
+            _controller = controller;
+        }
+        public void Setup(int pin , PinMode mode ) { 
+            _controller.OpenPin(pin, mode);
+        }
+        public void Write(int pin , bool value) {
+            if(value)
+                _controller.Write(pin, PinValue.High);
+            else
+                _controller.Write(pin, PinValue.High);
+
+        }
+        public bool Read(int pin)
+        {
+          return _controller.Read(pin)== PinValue.High ?true:false;
+        }
+    }
+}
