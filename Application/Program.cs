d4dea6a620f91abbe6ffed5f8b110b63f600d380
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-01-11 21:19:02 +0300

    commit

--- Application/Program.cs
+++ Application/Program.cs
@@ -32,8 +32,15 @@ var mcp23017x20 = new Mcp23017(i2cDevicex20);
 // Set the I/O direction for PortA and PortB (0 means output, 1 means input)
 mcp23017x20.WriteByte(Register.IODIR, 0b0000_0000, Port.PortA);
 mcp23017x20.WriteByte(Register.IODIR, 0b0000_0000, Port.PortB);
+while (true) {
+    mcp23017x20.WriteByte(Register.GPIO, 0b0000_0011, Port.PortA);
+    Task.Delay(1000).Wait();
 
-mcp23017x20.WriteByte(Register.GPIO, 0b0000_0011, Port.PortA);
+    mcp23017x20.WriteByte(Register.GPIO, 0b0000_0000, Port.PortA);
+    Task.Delay(1000).Wait();
+
+}
+
 
 
 
