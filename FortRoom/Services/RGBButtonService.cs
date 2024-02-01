5e05cee5dc2643f77f75502e1316ef003268d559
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-02-01 22:52:01 +0300

    Commit

--- FortRoom/Services/RGBButtonService.cs
+++ FortRoom/Services/RGBButtonService.cs
@@ -59,8 +59,8 @@ namespace FortRoom.Services
 
             while (!cancellationToken.IsCancellationRequested)
             {
-                RGBButtonList[0].TurnColorOn(RGBColor.Green);
-                return;
+                //RGBButtonList[0].TurnColorOn(RGBColor.Green);
+                //return;
                 if (VariableControlService.IsTheGameStarted)
                 {
                     if (!activeButton && timerToStart.ElapsedMilliseconds > randomTime)
