a74a0c536c7ce95c7d2f19a525a0b56a14056206
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-02-02 17:37:04 +0300

    commit

--- Application/Program.cs
+++ Application/Program.cs
@@ -29,7 +29,7 @@ using Library.RGBLib;
 using Library.PinMapping;
 
 // === RGB Strip
-RGBLight.Init(MasterOutputPin.Data, MasterOutputPin.Clk);
+RGBLight.Init(MasterOutputPin.Clk, MasterOutputPin.Data);
 while (true) {
     Console.WriteLine("Red");
     RGBLight.SetColor(RGBColor.Red);
