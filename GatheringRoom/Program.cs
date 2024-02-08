148071a113be0f92c8bf9214de447c9b74bff62f
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-02-08 20:27:01 +0300

    commit

--- GatheringRoom/Program.cs
+++ GatheringRoom/Program.cs
@@ -4,7 +4,10 @@ using Microsoft.Extensions.Hosting;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -23,10 +26,7 @@ builder.Services.AddSwaggerGen();
 //                   .AllowAnyHeader();
 //        });
 //});
-builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
-{
-    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
-}));
+
 var app = builder.Build();
 app.UseCors("corsapp");
 
