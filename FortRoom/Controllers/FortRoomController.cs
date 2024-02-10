85ddea15d54096b04ff743cd6e374413ef0588ad
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-02-10 14:49:40 +0300

    commit

--- FortRoom/Controllers/FortRoomController.cs
+++ FortRoom/Controllers/FortRoomController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FortRoom.Controllers
 {
-    [EnableCors("AllowAll")]
+    //[EnableCors("AllowAll")]
     [Route("api/[controller]")]
     [ApiController]
     public class FortRoomController : ControllerBase
