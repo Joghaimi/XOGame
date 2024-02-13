e99fc9b013695bae12a1fb9f7d1bbc9cf90ebcff
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-02-13 21:29:25 +0300

    commit

--- FortRoom/Program.cs
+++ FortRoom/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHostedService<MainService>(); // The Main Flow .. 
 builder.Services.AddHostedService<RGBButtonService>(); // The Main Flow .. 
-//builder.Services.AddHostedService<PressureMatService>(); // The Main Flow .. 
+builder.Services.AddHostedService<PressureMatService>(); // The Main Flow .. 
 //builder.Services.AddHostedService<ObstructionControlService>(); // The Main Flow .. 
 
 // Add services to the container.
