25983b34444d7e1eecfa1b865dbaf69e585437a7
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-02-16 22:34:27 +0300

    commit

--- FortRoom/Services/RGBButtonService.cs
+++ FortRoom/Services/RGBButtonService.cs
@@ -24,33 +24,29 @@ namespace FortRoom.Services
         public Task StartAsync(CancellationToken cancellationToken)
         {
             AudioPlayer.PIBackgroundSound(SoundType.Background);
-
+            // 5 6 9 10 11 12 13 14 15 16
+            // 9 10 11 12 5 13 14 15 16 6
             // TO DO Init The RGB Light .. 
-            Console.WriteLine("RGB 1");
-            RGBButtonList.Add(new RGBButton(RGBButtonPin.RGBR1, RGBButtonPin.RGBG1, RGBButtonPin.RGBB1, RGBButtonPin.RGBPB1));
-            Console.WriteLine("RGB 2");
-            RGBButtonList.Add(new RGBButton(RGBButtonPin.RGBR2, RGBButtonPin.RGBG2, RGBButtonPin.RGBB2, RGBButtonPin.RGBPB2));
-            Console.WriteLine("RGB 3");
-            RGBButtonList.Add(new RGBButton(RGBButtonPin.RGBR3, RGBButtonPin.RGBG3, RGBButtonPin.RGBB3, RGBButtonPin.RGBPB3));
-            Console.WriteLine("RGB 4");
-            RGBButtonList.Add(new RGBButton(RGBButtonPin.RGBR4, RGBButtonPin.RGBG4, RGBButtonPin.RGBB4, RGBButtonPin.RGBPB4));
             Console.WriteLine("RGB 5");
             RGBButtonList.Add(new RGBButton(RGBButtonPin.RGBR5, RGBButtonPin.RGBG5, RGBButtonPin.RGBB5, RGBButtonPin.RGBPB5));
             Console.WriteLine("RGB 6");
             RGBButtonList.Add(new RGBButton(RGBButtonPin.RGBR6, RGBButtonPin.RGBG6, RGBButtonPin.RGBB6, RGBButtonPin.RGBPB6));
-            Console.WriteLine("RGB 7");
-            RGBButtonList.Add(new RGBButton(RGBButtonPin.RGBR7, RGBButtonPin.RGBG7, RGBButtonPin.RGBB7, RGBButtonPin.RGBPB7));
-            Console.WriteLine("RGB 8");
-            RGBButtonList.Add(new RGBButton(RGBButtonPin.RGBR8, RGBButtonPin.RGBG8, RGBButtonPin.RGBB8, RGBButtonPin.RGBPB8));
             Console.WriteLine("RGB 9");
             RGBButtonList.Add(new RGBButton(RGBButtonPin.RGBR9, RGBButtonPin.RGBG9, RGBButtonPin.RGBB9, RGBButtonPin.RGBPB9));
             Console.WriteLine("RGB 10");
             RGBButtonList.Add(new RGBButton(RGBButtonPin.RGBR10, RGBButtonPin.RGBG10, RGBButtonPin.RGBB10, RGBButtonPin.RGBPB10));
-            //Console.WriteLine("RGB 11");
-            //RGBButtonList.Add(new RGBButton(RGBButtonPin.RGBR11, RGBButtonPin.RGBG11, RGBButtonPin.RGBB11, RGBButtonPin.RGBPB11));
-            //Console.WriteLine("RGB 12");
-            //RGBButtonList.Add(new RGBButton(RGBButtonPin.RGBR12, RGBButtonPin.RGBG12, RGBButtonPin.RGBB12, RGBButtonPin.RGBPB12));
-            //Console.WriteLine("=======================");
+            Console.WriteLine("RGB 11");
+            RGBButtonList.Add(new RGBButton(RGBButtonPin.RGBR11, RGBButtonPin.RGBG11, RGBButtonPin.RGBB11, RGBButtonPin.RGBPB11));
+            Console.WriteLine("RGB 12");
+            RGBButtonList.Add(new RGBButton(RGBButtonPin.RGBR12, RGBButtonPin.RGBG12, RGBButtonPin.RGBB12, RGBButtonPin.RGBPB12));
+            Console.WriteLine("RGB 13");
+            RGBButtonList.Add(new RGBButton(RGBButtonPin.RGBR13, RGBButtonPin.RGBG13, RGBButtonPin.RGBB13, RGBButtonPin.RGBPB13));
+            Console.WriteLine("RGB 14");
+            RGBButtonList.Add(new RGBButton(RGBButtonPin.RGBR14, RGBButtonPin.RGBG14, RGBButtonPin.RGBB14, RGBButtonPin.RGBPB14));
+            Console.WriteLine("RGB 15");
+            RGBButtonList.Add(new RGBButton(RGBButtonPin.RGBR15, RGBButtonPin.RGBG15, RGBButtonPin.RGBB15, RGBButtonPin.RGBPB15));
+            Console.WriteLine("RGB 16");
+            RGBButtonList.Add(new RGBButton(RGBButtonPin.RGBR16, RGBButtonPin.RGBG16, RGBButtonPin.RGBB16, RGBButtonPin.RGBPB16));
 
 
             _cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
