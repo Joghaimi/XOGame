7ae0d816d36e0aef656fb1f2046a4e3e8d04c41c
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-02-16 22:55:48 +0300

    commit

--- FortRoom/Services/ObstructionControlService.cs
+++ FortRoom/Services/ObstructionControlService.cs
@@ -56,8 +56,8 @@ namespace FortRoom.Services
 
 
             Modbus.WriteSingleRegister((byte)ModbusSlave.Slave1, (int)ModbusAddress.startStop, (int)MotorStatus.Run);
-            Modbus.WriteSingleRegister((byte)ModbusSlave.Slave2, (int)ModbusAddress.startStop, (int)MotorStatus.Run);
-            //Modbus.WriteSingleRegister((byte)ModbusSlave.Slave3, (int)ModbusAddress.startStop, (int)MotorStatus.Run);
+            Modbus.WriteSingleRegister((byte)ModbusSlave.Slave2, (int)ModbusAddress.startStop, (int)MotorStatus.Reverse);
+            //Modbus.WriteSingleRegister((byte)ModbusSlave.Slave3, (int)ModbusAddress.startStop, (int)MotorStatus.Reverse);
             Modbus.WriteSingleRegister((byte)ModbusSlave.Slave4, (int)ModbusAddress.startStop, (int)MotorStatus.Run);
 
 
