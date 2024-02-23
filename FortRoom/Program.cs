eb5622572ee314763e2345d08ccd6df2e9628f0e
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-02-23 20:27:46 +0300

    commit

--- FortRoom/Program.cs
+++ FortRoom/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddLogging(builder => builder.AddConsole());
+builder.Services.AddLogging(builder => builder.AddConsole().SetMinimumLevel(LogLevel.Trace));
 
 builder.Services.AddHostedService<MainService>(); // The Main Flow .. 
 builder.Services.AddHostedService<RGBButtonService>(); // The Main Flow .. 
