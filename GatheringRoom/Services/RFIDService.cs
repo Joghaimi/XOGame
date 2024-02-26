d611daa892f4b9eb030af28ee3914540b39aa5ec
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-02-26 19:14:56 +0300

    git

--- GatheringRoom/Services/RFIDService.cs
+++ GatheringRoom/Services/RFIDService.cs
@@ -31,45 +31,47 @@ namespace GatheringRoom.Services
         {
             while (!cancellationToken.IsCancellationRequested)
             {
-                if (_rfidController.CheckCardExisting() && VariableControlService.TeamScore.player.Count < 5 && !stop)
+                if (_rfidController.CheckCardExisting() && VariableControlService.TeamScore.player.Count > 5 && !stop)
                 {
-                    _logger.LogTrace("New Card Found");
+                    _logger.LogWarning("New Card Found");
                     string newPlayerId = _rfidController.ReadCardInfo();
                     bool isInTeam = VariableControlService.TeamScore.player.Any(item => item.Id == newPlayerId);
                     bool hasId = !string.IsNullOrEmpty(newPlayerId);
-                    if (hasId && !isInTeam)
-                    {
-                        using (HttpClient httpClient = new HttpClient())
-                        {
-                            string apiUrl = "https://thcyle7652.execute-api.us-east-1.amazonaws.com/default/myservice-dev-hello";
-                            string jsonData = "{\"rfid\":" + newPlayerId + "}";
-                            StringContent content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
-                            HttpResponseMessage response = await httpClient.PostAsync(apiUrl, content);
-                            if (response.IsSuccessStatusCode)
-                            {
-                                string responseContent = await response.Content.ReadAsStringAsync();
-                                List<Person> people = JsonConvert.DeserializeObject<List<Person>>(responseContent);
-                                if (people.Count > 0)
-                                {
-                                    var player = new Player
-                                    {
-                                        Id = newPlayerId,
-                                        FirstName = people[0].firstname,
-                                        LastName = people[0].lastname
-                                    };
-                                    VariableControlService.TeamScore.player.Add(player);
-                                }
-                                _logger.LogError($"POST request successful {responseContent}. Response: {people[0].firstname} {people[0].lastname}");
-                            }
-                            else
-                                _logger.LogError($"POST request failed. Status Code: {response.StatusCode}");
-                        }
-                    }
-                    else
-                        _logger.LogWarning($"Player is Exist :{isInTeam} or id is null {!hasId} ");
+                    _logger.LogWarning("Id" + newPlayerId);
+
+                    //if (hasId && !isInTeam)
+                    //{
+                    //    using (HttpClient httpClient = new HttpClient())
+                    //    {
+                    //        string apiUrl = "https://thcyle7652.execute-api.us-east-1.amazonaws.com/default/myservice-dev-hello";
+                    //        string jsonData = "{\"rfid\":" + newPlayerId + "}";
+                    //        StringContent content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+                    //        HttpResponseMessage response = await httpClient.PostAsync(apiUrl, content);
+                    //        if (response.IsSuccessStatusCode)
+                    //        {
+                    //            string responseContent = await response.Content.ReadAsStringAsync();
+                    //            List<Person> people = JsonConvert.DeserializeObject<List<Person>>(responseContent);
+                    //            if (people.Count > 0)
+                    //            {
+                    //                var player = new Player
+                    //                {
+                    //                    Id = newPlayerId,
+                    //                    FirstName = people[0].firstname,
+                    //                    LastName = people[0].lastname
+                    //                };
+                    //                VariableControlService.TeamScore.player.Add(player);
+                    //            }
+                    //            _logger.LogError($"POST request successful {responseContent}. Response: {people[0].firstname} {people[0].lastname}");
+                    //        }
+                    //        else
+                    //            _logger.LogError($"POST request failed. Status Code: {response.StatusCode}");
+                    //    }
+                    //}
+                    //else
+                    //    _logger.LogWarning($"Player is Exist :{isInTeam} or id is null {!hasId} ");
                 }
-                else
-                    _logger.LogWarning("Can't Have More Than 5 Member in a team");
+                //else
+                //_logger.LogWarning("Can't Have More Than 5 Member in a team");
                 await Task.Delay(TimeSpan.FromMilliseconds(1000), cancellationToken);
             }
         }
