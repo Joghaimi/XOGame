405e7be08fe527b81f4a07a66e31ffa19659b0ae
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-02-26 20:12:21 +0300

    commit

--- GatheringRoom/Services/RFIDService.cs
+++ GatheringRoom/Services/RFIDService.cs
@@ -96,5 +96,7 @@ class Person
 {
     public string firstname { get; set; }
     public string lastname { get; set; }
+    public string mobilenumber { get; set; }
+    public string email { get; set; }
 }
 
