cad6cbced53859353812c4125d8ca795f6ad2f86
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-02-26 20:32:36 +0300

    commit

--- GatheringRoom/Services/RFIDService.cs
+++ GatheringRoom/Services/RFIDService.cs
@@ -22,18 +22,57 @@ namespace GatheringRoom.Services
         {
             _logger.LogInformation("Start RFID Service");
             _cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
-            _rfidController.Init(pinReset);
+            //_rfidController.Init(pinReset);
             Task.Run(() => RunService(_cts.Token));
             return Task.CompletedTask;
         }
 
         private async Task RunService(CancellationToken cancellationToken)
         {
+            _logger.LogDebug("New Card Found");
+            string newPlayerId = _rfidController.ReadCardInfo();
+            bool isInTeam = VariableControlService.TeamScore.player.Any(item => item.Id == newPlayerId);
+            bool hasId = !string.IsNullOrEmpty(newPlayerId);
+            if (hasId && !isInTeam)
+            {
+
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    string apiUrl = "https://thcyle7652.execute-api.us-east-1.amazonaws.com/default/myservice-dev-hello";
+                    string jsonData = "{\"rfid\":\"" + newPlayerId + "\"}";
+
+                    StringContent content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = await httpClient.PostAsync(apiUrl, content);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseContent = await response.Content.ReadAsStringAsync();
+                        List<Person> people = JsonConvert.DeserializeObject<List<Person>>(responseContent);
+                        if (people.Count > 0)
+                        {
+                            var player = new Player
+                            {
+                                Id = newPlayerId,
+                                FirstName = people[0].firstname,
+                                LastName = people[0].lastname
+                            };
+                            VariableControlService.TeamScore.player.Add(player);
+                            Console.WriteLine(people[0].firstname);
+                            Console.WriteLine(people[0].lastname);
+                        }
+                        _logger.LogError($"POST request successful {responseContent}. Response: {people[0].firstname} {people[0].lastname}");
+                    }
+                    else
+                        _logger.LogError($"POST request failed. Status Code: {response.StatusCode}");
+                }
+            }
+            else
+                _logger.LogWarning($"Player is Exist :{isInTeam} or id is null {!hasId} ");
+            return;
             while (!cancellationToken.IsCancellationRequested)
             {
                 if (_rfidController.CheckCardExisting())
                 {
-                    if ( VariableControlService.TeamScore.player.Count < 5 && !stop)
+                    if (VariableControlService.TeamScore.player.Count < 5 && !stop)
                     {
                         _logger.LogDebug("New Card Found");
                         string newPlayerId = _rfidController.ReadCardInfo();
