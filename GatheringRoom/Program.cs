e273a81e3912a6fb8be556c4585288c93ab2baf5
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-02-27 21:00:48 +0300

    ؤؤ

--- GatheringRoom/Program.cs
+++ GatheringRoom/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddLogging(builder => builder.AddConsole().SetMinimumLevel(LogLevel.Trace));
-builder.Services.AddHostedService<RFIDService>();
+//builder.Services.AddHostedService<RFIDService>();
 builder.Services.AddHostedService<RoomSensorServices>();
 var app = builder.Build();
 app.UseCors("corsapp");
