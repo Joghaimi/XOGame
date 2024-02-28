958f567da7fa5522ea4b73e3eb7e014aee6b761e
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-02-28 21:44:25 +0300

    dd

--- DivingRoom/Services/RGBButtonService.cs
+++ DivingRoom/Services/RGBButtonService.cs
@@ -23,7 +23,7 @@ namespace DivingRoom.Services
         int numberOfPressedButton = 0;
         int Score = 0;
         int currentPeriod = 30000;
-        int CurrentColor = 4;
+        int CurrentColor = 5;
         int difficulty = 6;
         //int[] unSelectedPushButton = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         List<int> unSelectedPushButton = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
