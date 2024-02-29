10e2ad031df580013ddbac2b9cd1dcb28d009752
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-02-29 22:10:25 +0300

    cc

--- FloorIsLava/Services/RGBButtonService.cs
+++ FloorIsLava/Services/RGBButtonService.cs
@@ -50,50 +50,49 @@ namespace FloorIsLava.Services
         {
             while (true)
             {
-                Console.WriteLine(!RGBButtonList[0].CurrentStatus());
-                Thread.Sleep(1000);
 
 
-                //if (!MCP23Controller.Read(MasterDI.IN2.Chip, MasterDI.IN2.port, MasterDI.IN2.PinNumber) && !IN2)
-                //{
-                //    IN2 = true;
-                //    RGBLight.SetColor(RGBColor.Green);
-                //    RGBLight.TurnRGBOFFDelayed();
-                //    Console.WriteLine("====");
-                //}
-                //if (!MCP23Controller.Read(MasterDI.IN3.Chip, MasterDI.IN3.port, MasterDI.IN3.PinNumber) && !IN3)
-                //{
-                //    IN3 = true;
-                //    RGBLight.SetColor(RGBColor.Green);
-                //    RGBLight.TurnRGBOFFDelayed();
-                //    Console.WriteLine("====");
+                if (!MCP23Controller.Read(MasterDI.IN2.Chip, MasterDI.IN2.port, MasterDI.IN2.PinNumber) && !IN2)
+                {
+                    IN2 = true;
+                    RGBLight.SetColor(RGBColor.Green);
+                    RGBLight.TurnRGBOFFDelayed();
+                    Console.WriteLine("====");
+                }
+                if (!MCP23Controller.Read(MasterDI.IN3.Chip, MasterDI.IN3.port, MasterDI.IN3.PinNumber) && !IN3)
+                {
+                    IN3 = true;
+                    RGBLight.SetColor(RGBColor.Green);
+                    RGBLight.TurnRGBOFFDelayed();
+                    Console.WriteLine("====");
 
-                //}
-                //if (!MCP23Controller.Read(MasterDI.IN4.Chip, MasterDI.IN4.port, MasterDI.IN4.PinNumber) && !IN4)
-                //{
-                //    IN4 = true;
-                //    RGBLight.SetColor(RGBColor.Green);
-                //    RGBLight.TurnRGBOFFDelayed();
-                //    Console.WriteLine("====");
+                }
+                if (!MCP23Controller.Read(MasterDI.IN4.Chip, MasterDI.IN4.port, MasterDI.IN4.PinNumber) && !IN4)
+                {
+                    IN4 = true;
+                    RGBLight.SetColor(RGBColor.Green);
+                    RGBLight.TurnRGBOFFDelayed();
+                    Console.WriteLine("====");
 
-                //}
-                //if (IN2 && IN3 && IN4)
-                //{
-                //    Console.WriteLine("Pressed all 3");
+                }
+                if (IN2 && IN3 && IN4)
+                {
+                    Console.WriteLine("Pressed all 3");
 
-                //    MCP23Controller.Write(MasterOutputPin.OUTPUT5.Chip, MasterOutputPin.OUTPUT5.port, MasterOutputPin.OUTPUT5.PinNumber, PinState.High);
-                //    while (RGBButtonList[0].CurrentStatus())
-                //    {
-                //        Thread.Sleep(10);
-                //    }
-                //    Console.WriteLine("Button Pressed");
+                    MCP23Controller.Write(MasterOutputPin.OUTPUT5.Chip, MasterOutputPin.OUTPUT5.port, MasterOutputPin.OUTPUT5.PinNumber, PinState.High);
+                    while (true)
+                    {
+                        Console.WriteLine(!RGBButtonList[0].CurrentStatus());
+                        Thread.Sleep(100);
+                    }
+                    Console.WriteLine("Button Pressed");
 
-                //    MCP23Controller.Write(MasterOutputPin.OUTPUT5.Chip, MasterOutputPin.OUTPUT5.port, MasterOutputPin.OUTPUT5.PinNumber, PinState.Low);
-                //    RGBButtonList[0].TurnColorOn(RGBColor.Blue);
-                //    IN2 = false;
-                //    IN3 = false;
-                //    IN4 = false;
-                //}
+                    MCP23Controller.Write(MasterOutputPin.OUTPUT5.Chip, MasterOutputPin.OUTPUT5.port, MasterOutputPin.OUTPUT5.PinNumber, PinState.Low);
+                    RGBButtonList[0].TurnColorOn(RGBColor.Blue);
+                    IN2 = false;
+                    IN3 = false;
+                    IN4 = false;
+                }
 
 
             }
