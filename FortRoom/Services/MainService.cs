87d497cdd16a48732f7d2b62255a83b058a2ffce
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-03-22 22:11:36 +0300

    xx

--- FortRoom/Services/MainService.cs
+++ FortRoom/Services/MainService.cs
@@ -24,7 +24,7 @@ namespace FortRoom.Services
             _controller = new GPIOController();
             RGBLight.Init(MasterOutputPin.Clk, MasterOutputPin.Data, Room.Fort);
             AudioPlayer.Init(Room.Fort);
-            MCP23Controller.Init(Room.Fort);
+            MCP23Controller.Init(true);
             // Init the Pin's
             _controller.Setup(MasterDI.PIRPin1, PinMode.InputPullDown);
             _controller.Setup(MasterDI.PIRPin2, PinMode.InputPullDown);
