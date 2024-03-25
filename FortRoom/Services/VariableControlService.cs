6c209acc105381508b662e7c41e1489fc7d2bb38
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-03-25 22:43:00 +0300

    fix variables

--- FortRoom/Services/VariableControlService.cs
+++ FortRoom/Services/VariableControlService.cs
@@ -7,12 +7,12 @@ namespace FortRoom.Services
 {
     public static class VariableControlService
     {
-        public static bool IsTheGameStarted { get; set; } = true;
+        public static bool IsTheGameStarted { get; set; } = false;
         public static bool IsTheGameFinished { get; set; } = false;
         public static bool IsTheirAnyOneInTheRoom { get; set; } = false;
         public static int TimeOfPressureHit { get; set; } = 0;
         public static int ActiveButtonPressed { get; set; } = 0;
         public static Team TeamScore { get; set; } = new Team();
-        public static bool IsOccupied { get; set; } = true;
+        public static bool IsOccupied { get; set; }
     }
 }
