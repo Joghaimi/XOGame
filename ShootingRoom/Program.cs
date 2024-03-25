732009a88fa2577c871ee824023e151ed37c65dd
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-03-25 23:52:10 +0300

    cc

--- ShootingRoom/Program.cs
+++ ShootingRoom/Program.cs
@@ -1,8 +1,11 @@
 
 
 using ShootingRoom.Services;
-
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
 
 // Add services to the container.
 
