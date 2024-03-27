7242e4537b360af75aca5259fa65aa5fb36078e2
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-03-28 00:02:29 +0300

    cc

--- FloorIsLava/Services/PressureMatService.cs
+++ FloorIsLava/Services/PressureMatService.cs
@@ -35,7 +35,7 @@ namespace FloorIsLava.Services
             {
                 if (VariableControlService.IsTheGameStarted)
                 {
-                    currentValue = !MCP23Controller.Read(MasterDI.IN1,MasterDI.IN1.PinNumber);
+                    currentValue = !MCP23Controller.Read(MasterDI.IN1);
                     if (currentValue && !scoreJustDecreased)
                     {
 
