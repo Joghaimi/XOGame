3ee13a245518f30bc59c15ceb350c97a3f20269f
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-03-30 23:10:03 +0300

    cc

--- DivingRoom/Services/MainService.cs
+++ DivingRoom/Services/MainService.cs
@@ -138,6 +138,7 @@ namespace DivingRoom.Services
             VariableControlService.IsTheGameStarted = false;
             VariableControlService.IsTheGameFinished = true;
             //VariableControlService.EnableGoingToTheNextRoom = true;
+            _logger.LogInformation("Stop The Game");
 
         }
         private void ResetTheGame()
