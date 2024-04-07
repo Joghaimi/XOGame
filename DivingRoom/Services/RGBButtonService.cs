c156d1a527e8b86d220dd87eb10172728a772539
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-04-08 00:50:12 +0300

    cc

--- DivingRoom/Services/RGBButtonService.cs
+++ DivingRoom/Services/RGBButtonService.cs
@@ -222,7 +222,7 @@ namespace DivingRoom.Services
 
                 foreach (var item in unSelectedPushButton)
                 {
-                    int randomNumber = random.Next(0, unSelectedColorArray.Count);
+                    int randomNumber = random.Next(0, unSelectedColorArray.Length);
                     _logger.LogTrace($"other color {item} With Color {unSelectedColorArray[randomNumber]}");
                     RGBButtonList[item].TurnColorOn(unSelectedColorArray[randomNumber]);
                 }
