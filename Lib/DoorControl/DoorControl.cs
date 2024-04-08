8167d941cba86442330df5c5c0e4d9f7480749df
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-04-08 04:35:59 +0300

    cc

--- Lib/DoorControl/DoorControl.cs
+++ Lib/DoorControl/DoorControl.cs
@@ -8,15 +8,16 @@ namespace Library.DoorControl
     {
         public static void Status(MCP23Pin doorPin, bool status)
         {
+            Console.WriteLine($"Door Status {status}");
             if (!status)
             {
                 MCP23Controller.PinModeSetup(doorPin, PinMode.Output);
-                MCP23Controller.Write(doorPin, PinState.High);
+                //MCP23Controller.Write(doorPin, PinState.High);
             }
             else
             {
                 MCP23Controller.PinModeSetup(doorPin, PinMode.Input);
-                MCP23Controller.Write(doorPin, PinState.Low);
+                //MCP23Controller.Write(doorPin, PinState.Low);
             }
         }
     }
