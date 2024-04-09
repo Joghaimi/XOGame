1e6429639aa00df79d2986da12509c11f333e734
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-04-10 01:56:33 +0300

    cc

--- DarkRoom/Services/VariableControlService.cs
+++ DarkRoom/Services/VariableControlService.cs
@@ -34,7 +34,6 @@ namespace DarkRoom.Services
         public static DoorStatus CurrentDoorStatus { get; set; } = DoorStatus.Open;
         public static DoorStatus NewDoorStatus { get; set; } = DoorStatus.Open;
 
-
         public static string NextRoomURL = "https://floor.local:7248/api/floorislava/RoomStatus";
         public static string SendScoreToTheNextRoom = "https://floor.local:7248/api/floorislava/ReceiveScore";
 
