4361247d987dcdc8c7618f33c084244f108ef9fc
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-04-10 02:48:32 +0300

    cc

--- FloorIsLava/Services/MainService.cs
+++ FloorIsLava/Services/MainService.cs
@@ -76,6 +76,14 @@ namespace FloorIsLava.Services
         }
         private async Task RunService(CancellationToken cancellationToken)
         {
+            while (true)
+            {
+                RelayController.Status(EnterRGBButton, true);
+                Thread.Sleep(1000);
+                RelayController.Status(EnterRGBButton, false);
+                Thread.Sleep(1000);
+
+            }
 
             while (!cancellationToken.IsCancellationRequested)
             {
