8e2e2ccacf26aefc52b4285b4b2622566312d3dc
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-04-09 17:36:12 +0300

    e

--- FortRoom/Services/MainService.cs
+++ FortRoom/Services/MainService.cs
@@ -21,11 +21,11 @@ namespace FortRoom.Services
 
         bool EnterRGBButtonStatus = false;
         private MCP23Pin EnterRGBButton = MasterOutputPin.OUTPUT8;
-        private MCP23Pin EnterRoomPB = MasterDI.IN6;
+        private MCP23Pin EnterRoomPB = MasterDI.IN5;
 
         bool NextRoomRGBButtonStatus = false;
         private MCP23Pin NextRoomPBLight = MasterOutputPin.OUTPUT8;
-        private MCP23Pin NextRoomPB = MasterDI.IN6;
+        private MCP23Pin NextRoomPB = MasterDI.IN5;
 
 
         private CancellationTokenSource _cts, _cts2, _cts3, _cts4;
