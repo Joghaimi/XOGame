faf63f8634b9ce65bd5636951a6560c046167b54
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-04-11 16:39:05 +0300

    cc

--- Lib/GPIOLib/MCP23Controller.cs
+++ Lib/GPIOLib/MCP23Controller.cs
@@ -277,7 +277,8 @@ namespace Library.GPIOLib
             }
             catch(Exception ex)
             {
-                throw new ArgumentException($"Read MCPChip Exception {ex.Message}");
+                return false;
+                //throw new ArgumentException($"Read MCPChip Exception {ex.Message}");
             }
             // Test 
             //byte GPIOStatus = ReturnMCPChip(_MCP23Pin.Chip).ReadByte(Register.GPIO, _MCP23Pin.port);
