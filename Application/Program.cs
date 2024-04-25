7529ee6329e8077567b04a741d23ae78442d238f
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-04-25 23:05:45 +0300

    cc

--- Application/Program.cs
+++ Application/Program.cs
@@ -39,42 +39,18 @@ test();
 
 void test()
 {
-    //Runtime.PythonDLL = "C:\\Python312\\python312.dll";
-    //PythonEngine.Initialize();
-    //using (Py.GIL())
-    //{
-    //    var python = Py.Import("ahmad");
-    //    var iscallable = python.IsCallable();
-    //    python.InvokeMethod("print_ahmad");
-    //    python.InvokeMethod("print_ahmad");
-    //    python.InvokeMethod("print_ahmad");
-    //    python.InvokeMethod("print_ahmad");
-    //    python.InvokeMethod("print_ahmad");
-    //}
-
-
+    Runtime.PythonDLL = "/usr/lib/python3.11/config-3.11-aarch64-linux-gnu/python.o";
     PythonEngine.Initialize();
     using (Py.GIL())
     {
-        dynamic np = Py.Import("numpy");
-        Console.WriteLine(np.cos(np.pi * 2));
-
-        dynamic sin = np.sin;
-        Console.WriteLine(sin(5));
-
-        double c = (double)(np.cos(5) + sin(5));
-        Console.WriteLine(c);
-
-        dynamic a = np.array(new List<float> { 1, 2, 3 });
-        Console.WriteLine(a.dtype);
-
-        dynamic b = np.array(new List<float> { 6, 5, 4 }, dtype: np.int32);
-        Console.WriteLine(b.dtype);
-
-        Console.WriteLine(a * b);
-        Console.ReadKey();
+        var python = Py.Import("ahmad");
+        var iscallable = python.IsCallable();
+        python.InvokeMethod("print_ahmad");
+        python.InvokeMethod("print_ahmad");
+        python.InvokeMethod("print_ahmad");
+        python.InvokeMethod("print_ahmad");
+        python.InvokeMethod("print_ahmad");
     }
-
 }
 
 
