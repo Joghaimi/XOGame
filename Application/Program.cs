05834e5e07491ad0b401b7fc2eab008c82907e4c
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-04-25 22:52:18 +0300

    test

--- Application/Program.cs
+++ Application/Program.cs
@@ -40,17 +40,41 @@ test();
 void test()
 {
     //Runtime.PythonDLL = "C:\\Python312\\python312.dll";
+    //PythonEngine.Initialize();
+    //using (Py.GIL())
+    //{
+    //    var python = Py.Import("ahmad");
+    //    var iscallable = python.IsCallable();
+    //    python.InvokeMethod("print_ahmad");
+    //    python.InvokeMethod("print_ahmad");
+    //    python.InvokeMethod("print_ahmad");
+    //    python.InvokeMethod("print_ahmad");
+    //    python.InvokeMethod("print_ahmad");
+    //}
+
+
     PythonEngine.Initialize();
     using (Py.GIL())
     {
-        var python = Py.Import("ahmad");
-        var iscallable = python.IsCallable();
-        python.InvokeMethod("print_ahmad");
-        python.InvokeMethod("print_ahmad");
-        python.InvokeMethod("print_ahmad");
-        python.InvokeMethod("print_ahmad");
-        python.InvokeMethod("print_ahmad");
+        dynamic np = Py.Import("numpy");
+        Console.WriteLine(np.cos(np.pi * 2));
+
+        dynamic sin = np.sin;
+        Console.WriteLine(sin(5));
+
+        double c = (double)(np.cos(5) + sin(5));
+        Console.WriteLine(c);
+
+        dynamic a = np.array(new List<float> { 1, 2, 3 });
+        Console.WriteLine(a.dtype);
+
+        dynamic b = np.array(new List<float> { 6, 5, 4 }, dtype: np.int32);
+        Console.WriteLine(b.dtype);
+
+        Console.WriteLine(a * b);
+        Console.ReadKey();
     }
+
 }
 
 
