7fc9b577217eb5fc321736d1d8bf5a8333d07bf0
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-04-26 20:20:37 +0300

    c

--- CatchyGame/Program.cs
+++ CatchyGame/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddHostedService<SenarioService>(); // The Main Flow ..
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
