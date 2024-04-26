e111fd75ace76fdf9fc40e7a2dd97f692448aa56
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-04-26 21:19:58 +0300

    cc

--- CatchyGame/Service/SenarioService.cs
+++ CatchyGame/Service/SenarioService.cs
@@ -23,32 +23,33 @@ namespace CatchyGame.Service
             RGBButtonPixel rGBButton2Pixel = new RGBButtonPixel(40, new RGBButton(RGBButtonPin.RGBR4, RGBButtonPin.RGBG4, RGBButtonPin.RGBB4, RGBButtonPin.RGBPB4));
             RGBButtonPixel rGBButton3Pixel = new RGBButtonPixel(23, new RGBButton(RGBButtonPin.RGBR5, RGBButtonPin.RGBG5, RGBButtonPin.RGBB5, RGBButtonPin.RGBPB5));
             RGBButtonPixel rGBButton4Pixel = new RGBButtonPixel(41, new RGBButton(RGBButtonPin.RGBR8, RGBButtonPin.RGBG8, RGBButtonPin.RGBB8, RGBButtonPin.RGBPB8));
-            //var rgb2 = new RGBButton(RGBButtonPin.RGBR4, RGBButtonPin.RGBG4, RGBButtonPin.RGBB4, RGBButtonPin.RGBPB4);
-            //var rgb3 = new RGBButton(RGBButtonPin.RGBR5, RGBButtonPin.RGBG5, RGBButtonPin.RGBB5, RGBButtonPin.RGBPB5);
-            //var rgb4 = new RGBButton(RGBButtonPin.RGBR8, RGBButtonPin.RGBG8, RGBButtonPin.RGBB8, RGBButtonPin.RGBPB8);
-            //while (true) {
-            //    Console.WriteLine("RED");
-            //    rgb1.TurnColorOn(RGBColor.Red);
-            //    rgb2.TurnColorOn(RGBColor.Red);
-            //    rgb3.TurnColorOn(RGBColor.Red);
-            //    rgb4.TurnColorOn(RGBColor.Red);
-            //    Thread.Sleep(2000);
-            //    Console.WriteLine("Green");
+            var rgb2 = new RGBButton(RGBButtonPin.RGBR4, RGBButtonPin.RGBG4, RGBButtonPin.RGBB4, RGBButtonPin.RGBPB4);
+            var rgb3 = new RGBButton(RGBButtonPin.RGBR5, RGBButtonPin.RGBG5, RGBButtonPin.RGBB5, RGBButtonPin.RGBPB5);
+            var rgb4 = new RGBButton(RGBButtonPin.RGBR8, RGBButtonPin.RGBG8, RGBButtonPin.RGBB8, RGBButtonPin.RGBPB8);
+            while (true)
+            {
+                Console.WriteLine("RED");
+                rgb1.TurnColorOn(RGBColor.Red);
+                //rgb2.TurnColorOn(RGBColor.Red);
+                //rgb3.TurnColorOn(RGBColor.Red);
+                //rgb4.TurnColorOn(RGBColor.Red);
+                Thread.Sleep(2000);
+                Console.WriteLine("Green");
 
-            //    rgb1.TurnColorOn(RGBColor.Green);
-            //    rgb2.TurnColorOn(RGBColor.Green);
-            //    rgb4.TurnColorOn(RGBColor.Green);
-            //    rgb3.TurnColorOn(RGBColor.Green);
-            //    Thread.Sleep(2000);
-            //    Console.WriteLine("Blue");
+                rgb1.TurnColorOn(RGBColor.Green);
+                //rgb2.TurnColorOn(RGBColor.Green);
+                //rgb4.TurnColorOn(RGBColor.Green);
+                //rgb3.TurnColorOn(RGBColor.Green);
+                Thread.Sleep(2000);
+                Console.WriteLine("Blue");
 
-            //    rgb1.TurnColorOn(RGBColor.Blue);
-            //    rgb2.TurnColorOn(RGBColor.Blue);
-            //    rgb4.TurnColorOn(RGBColor.Blue);
-            //    rgb3.TurnColorOn(RGBColor.Blue);
-            //    Thread.Sleep(2000);
+                rgb1.TurnColorOn(RGBColor.Blue);
+                //rgb2.TurnColorOn(RGBColor.Blue);
+                //rgb4.TurnColorOn(RGBColor.Blue);
+                //rgb3.TurnColorOn(RGBColor.Blue);
+                Thread.Sleep(2000);
 
-            //}
+            }
 
 
             //Init Strip
@@ -75,17 +76,15 @@ namespace CatchyGame.Service
             {
                 RGBWS2811.SetColor(strip1.currentLed, strip1.rgbColor);
                 RGBWS2811.Commit();
-                strip1.NextLed();
-                if (strip1.rGBButton1.Pixel == strip1.currentLed)
-                {
-                    strip1.rGBButton1.Button.TurnColorOn(RGBColor.Red);
-                }
-                else
-                {
-                    rgb1.TurnColorOn(RGBColor.Blue);
-
-
-                }
+                //strip1.NextLed();
+                //if (strip1.rGBButton1.Pixel == strip1.currentLed)
+                //{
+                //    strip1.rGBButton1.Button.TurnColorOn(RGBColor.Red);
+                //}
+                //else
+                //{
+                //    rgb1.TurnColorOn(RGBColor.Blue);
+                //}
                 //rgb1
                 Thread.Sleep(1000);
                 Console.Write(".");
