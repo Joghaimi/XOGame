e394c7d5302566183f9820344268bccd62f941d2
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-04-26 10:18:41 +0300

    test

--- Application/Program.cs
+++ Application/Program.cs
@@ -49,7 +49,7 @@ void test()
         //PythonEngine.Exec("sys.path.append('/home/catchy/XOGame/Application')");
         var python = Py.Import("ahmad");
         //var iscallable = python.IsCallable();
-        //python.InvokeMethod("print_ahmad");
+        python.InvokeMethod("print_ahmad");
         //python.InvokeMethod("print_ahmad");
         //python.InvokeMethod("print_ahmad");
         //python.InvokeMethod("print_ahmad");
