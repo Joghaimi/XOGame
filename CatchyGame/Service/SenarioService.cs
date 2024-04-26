3c6a08bf4967de0bfe751eac16495756d9013282
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-04-26 21:05:15 +0300

    c

--- CatchyGame/Service/SenarioService.cs
+++ CatchyGame/Service/SenarioService.cs
@@ -16,13 +16,36 @@ namespace CatchyGame.Service
 
             MCP23Controller.Init(Room.Fort);
             // RGB Pixel Button 
-            RGBButtonPixel rGBButton1Pixel = new RGBButtonPixel(20, new RGBButton(RGBButtonPin.RGBR1, RGBButtonPin.RGBG1, RGBButtonPin.RGBB1, RGBButtonPin.RGBPB1));
-            RGBButtonPixel rGBButton2Pixel = new RGBButtonPixel(40, new RGBButton(RGBButtonPin.RGBR4, RGBButtonPin.RGBG4, RGBButtonPin.RGBB4, RGBButtonPin.RGBPB4));
-            RGBButtonPixel rGBButton3Pixel = new RGBButtonPixel(23, new RGBButton(RGBButtonPin.RGBR5, RGBButtonPin.RGBG5, RGBButtonPin.RGBB5, RGBButtonPin.RGBPB5));
-            RGBButtonPixel rGBButton4Pixel = new RGBButtonPixel(41, new RGBButton(RGBButtonPin.RGBR8, RGBButtonPin.RGBG8, RGBButtonPin.RGBB8, RGBButtonPin.RGBPB8));
+            //RGBButtonPixel rGBButton1Pixel = new RGBButtonPixel(20, new RGBButton(RGBButtonPin.RGBR1, RGBButtonPin.RGBG1, RGBButtonPin.RGBB1, RGBButtonPin.RGBPB1));
+            //RGBButtonPixel rGBButton2Pixel = new RGBButtonPixel(40, new RGBButton(RGBButtonPin.RGBR4, RGBButtonPin.RGBG4, RGBButtonPin.RGBB4, RGBButtonPin.RGBPB4));
+            //RGBButtonPixel rGBButton3Pixel = new RGBButtonPixel(23, new RGBButton(RGBButtonPin.RGBR5, RGBButtonPin.RGBG5, RGBButtonPin.RGBB5, RGBButtonPin.RGBPB5));
+            //RGBButtonPixel rGBButton4Pixel = new RGBButtonPixel(41, new RGBButton(RGBButtonPin.RGBR8, RGBButtonPin.RGBG8, RGBButtonPin.RGBB8, RGBButtonPin.RGBPB8));
+            var rgb1 = new RGBButton(RGBButtonPin.RGBR1, RGBButtonPin.RGBG1, RGBButtonPin.RGBB1, RGBButtonPin.RGBPB1);
+            var rgb2 = new RGBButton(RGBButtonPin.RGBR4, RGBButtonPin.RGBG4, RGBButtonPin.RGBB4, RGBButtonPin.RGBPB4);
+            var rgb3 = new RGBButton(RGBButtonPin.RGBR5, RGBButtonPin.RGBG5, RGBButtonPin.RGBB5, RGBButtonPin.RGBPB5);
+            var rgb4 = new RGBButton(RGBButtonPin.RGBR8, RGBButtonPin.RGBG8, RGBButtonPin.RGBB8, RGBButtonPin.RGBPB8);
+            while (true) {
+                rgb1.TurnColorOn(RGBColor.Red);
+                rgb2.TurnColorOn(RGBColor.Red);
+                rgb3.TurnColorOn(RGBColor.Red);
+                rgb4.TurnColorOn(RGBColor.Red);
+                Thread.Sleep(2000);
+                rgb1.TurnColorOn(RGBColor.Green);
+                rgb2.TurnColorOn(RGBColor.Green);
+                rgb4.TurnColorOn(RGBColor.Green);
+                rgb3.TurnColorOn(RGBColor.Green);
+                Thread.Sleep(2000);
+                rgb1.TurnColorOn(RGBColor.Blue);
+                rgb2.TurnColorOn(RGBColor.Blue);
+                rgb4.TurnColorOn(RGBColor.Blue);
+                rgb3.TurnColorOn(RGBColor.Blue);
+                Thread.Sleep(2000);
 
+            }
+            
+            
             // Init Strip
-            strip1 = new Strip(RGBColor.Yellow, 0, 89, rGBButton1Pixel, rGBButton2Pixel, rGBButton3Pixel, rGBButton4Pixel);
+            //strip1 = new Strip(RGBColor.Yellow, 0, 89, rGBButton1Pixel, rGBButton2Pixel, rGBButton3Pixel, rGBButton4Pixel);
 
 
             // Init the RGBStrip 
@@ -43,10 +66,11 @@ namespace CatchyGame.Service
         {
             while (!cancellationToken.IsCancellationRequested)
             {
-                RGBWS2811.SetColor(strip1.currentLed , strip1.rgbColor);
-                RGBWS2811.Commit();
-                strip1.NextLed();
-                Thread.Sleep(100);
+                //RGBWS2811.SetColor(strip1.currentLed , strip1.rgbColor);
+                //RGBWS2811.Commit();
+                //strip1.NextLed();
+                //rgb1
+                Thread.Sleep(1000);
                 Console.Write(".");
             }
         }
