c1f5453d230896a763c332ab5f6a9d585d8de388
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-05-28 22:05:09 +0300

    enable floor

--- FloorIsLava/Program.cs
+++ FloorIsLava/Program.cs
@@ -13,8 +13,8 @@ builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializ
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddHostedService<MainService>(); // The Main Flow .. 
-//builder.Services.AddHostedService<RGBButtonService>(); // The Main Flow .. 
+builder.Services.AddHostedService<MainService>(); // The Main Flow .. 
+builder.Services.AddHostedService<RGBButtonService>(); // The Main Flow .. 
 
 
 
