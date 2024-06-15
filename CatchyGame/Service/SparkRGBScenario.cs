8836c62fb9df4936824865462b892dfc7449ce2e
Ahmad joghaimi <ahmad.joghaimi@gmail.com>
2024-06-15 21:24:08 +0300

    Enable SparkRGBScenario

--- CatchyGame/Service/SparkRGBScenario.cs
+++ CatchyGame/Service/SparkRGBScenario.cs
@@ -42,11 +42,12 @@ namespace CatchyGame.Service
             PlayerTwoRGBButtonList.Add(new SparkRGBButton(new RGBButton(RGBButtonPin.RGBR14, RGBButtonPin.RGBG14, RGBButtonPin.RGBB14, RGBButtonPin.RGBPB14), 5, Library.RGBColor.Blue));
             PlayerTwoRGBButtonList.Add(new SparkRGBButton(new RGBButton(RGBButtonPin.RGBR15, RGBButtonPin.RGBG15, RGBButtonPin.RGBB15, RGBButtonPin.RGBPB15), 5, Library.RGBColor.Blue));
             PlayerTwoRGBButtonList.Add(new SparkRGBButton(new RGBButton(RGBButtonPin.RGBR16, RGBButtonPin.RGBG16, RGBButtonPin.RGBB16, RGBButtonPin.RGBPB16), 5, Library.RGBColor.Green));
+            RGBWS2811.Init();
 
-            //RGBWS2811.SetColorByRange(
-            //   VariableControlService.StripSevenStartIndex, VariableControlService.StripSevenEndIndex,
-            //   VariableControlService.StripSevenDefaultColor);
-            //RGBWS2811.Commit();
+            RGBWS2811.SetColorByRange(
+               VariableControlService.StripSevenStartIndex, VariableControlService.StripSevenEndIndex,
+               VariableControlService.StripSevenDefaultColor);
+            RGBWS2811.Commit();
 
 
 
